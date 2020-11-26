1f00a9ab1595d6ed7f9ab669298432c070afc4fb
qutianyu <qutianyu_ok@outlook.com>
2020-11-26 09:15:17 +0800

    20201126

---
+++ myths-common/src/main/java/cn/mikylin/myths/common/lang/Int.java
@@ -0,0 +1,60 @@
+package cn.mikylin.myths.common.lang;
+
+import java.util.Collection;
+
+/**
+ * int utils.
+ *
+ * @author mikylin
+ * @date 20201126
+ */
+public final class Int {
+
+    /**
+     * python : 循环 0 - 10
+     * for i in range(len(array)):
+     *    // ...
+     *
+     * like it :
+     * int[] is = new int[10];
+     * for (int i : Int.range(is)) {
+     *   // ...
+     * }
+     *
+     * for (int i : Int.range(0,10)) {
+     *    // ...
+     * }
+     */
+    public static int[] range(Collection coll) {
+        if (coll == null || coll.size() == 0)
+            return new int[0];
+        return range(0,coll.size() - 1);
+    }
+
+    public static int[] range(int[] is) {
+        if (is == null || is.length == 0)
+            return new int[0];
+        return range(0,is.length - 1);
+    }
+
+    public static int[] range(int end) {
+        return range(0,end);
+    }
+
+
+    public static int[] range(int begin,int end) {
+        if (end < begin)
+            return new int[0];
+        else if (end == begin)
+            return new int[]{begin};
+
+        int size = end - begin + 1;
+        int[] is = new int[size];
+        int j = 0;
+        for (int i = begin ; i <= end ; i ++) {
+            is[j ++] = i;
+        }
+        return is;
+    }
+
+}
